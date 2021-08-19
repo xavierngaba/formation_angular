207d177740081abd3f32b500e5610bf16d619a7e
Xavier Ngaba <xavierngaba@gmail.com>
2021-08-20 01:28:53 +0200

    add depedance injection service quick-lunch in main-dash

--- restaurangular/src/app/main-dash/main-dash.component.ts
+++ restaurangular/src/app/main-dash/main-dash.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointState, BreakpointObserver } from '@angular/cdk/layout';
+import { QuickLunchService } from '../services/quick-lunch.service';
+import { Food } from '../models/food.interface';
 
 @Component({
   selector: 'cc-main-dash',
   templateUrl: './main-dash.component.html',
   styleUrls: ['./main-dash.component.css']
 })
-export class MainDashComponent {
+export class MainDashComponent implements OnInit {
   /** Based on the screen size, switch from standard to one column per row */
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
@@ -29,5 +31,13 @@ export class MainDashComponent {
     })
   );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  burgers: Food[];
+  pizzas: Food[];
+
+  constructor(private breakpointObserver: BreakpointObserver, private qls: QuickLunchService) {}
+
+  ngOnInit() {
+    this.burgers = this.qls.getBurges();
+    this.pizzas = this.qls.getPizzas();
+  }
 }
