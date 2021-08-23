455aec3cfa449e229586e1ff61cd7af500ad1d41
Xavier Ngaba <xavierngaba@gmail.com>
2021-08-24 01:54:37 +0200

    add Module DatePicker Material and implement

--- restaurangular/src/app/app.module.ts
+++ restaurangular/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes, Router } from '@angular/router';
-import { ReactiveFormsModule} from '@angular/forms';
+import { ReactiveFormsModule, FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule, MatGridListModule, MatMenuModule, MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
 import { HomeComponent } from './home/home.component';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule, MatGridListModule, MatMenuModule, MatTableModule, MatPaginatorModule, MatSortModule, MatDatepickerModule, MatInputModule, MatFormFieldModule, MatNativeDateModule } from '@angular/material';
 import { CarteComponent } from './carte/carte.component';
 import { ReserveComponent } from './reserve/reserve.component';
 import { ContactComponent } from './contact/contact.component';
@@ -50,7 +50,12 @@ const routes: Routes = [
     MatPaginatorModule,
     MatSortModule,
     MatDividerModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- restaurangular/src/app/reserve/reserve.component.ts
+++ restaurangular/src/app/reserve/reserve.component.ts
@@ -14,14 +14,18 @@ export class ReserveComponent implements OnInit {
   timeOptions = ["12:00","12:30","13:00","13:30","19:00","19:30","20:00","20:30"];
   peopleOptions = [1,2,3,4,5,6,7,8];
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
     this.createForm();
   }
 
   createForm() {
-    
+    this.reservationForm = this.fb.group({
+      date: new Date(),
+      time: "",
+      people: 1
+    });
   }
 
 }
