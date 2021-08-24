f652c06a811863d53cddbfc1576b650ba1ed7a62
Xavier Ngaba <xavierngaba@gmail.com>
2021-08-24 02:14:29 +0200

    implement matSelect for the reservation page

--- restaurangular/src/app/app.module.ts
+++ restaurangular/src/app/app.module.ts
@@ -1,25 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes, Router } from '@angular/router';
-import { ReactiveFormsModule, FormsModule} from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { HomeComponent } from './home/home.component';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule, MatGridListModule, MatMenuModule, MatTableModule, MatPaginatorModule, MatSortModule, MatDatepickerModule, MatInputModule, MatFormFieldModule, MatNativeDateModule, MAT_DATE_LOCALE } from '@angular/material';
+import {
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule,
+  MatCardModule,
+  MatGridListModule,
+  MatMenuModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatDatepickerModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatNativeDateModule,
+  MAT_DATE_LOCALE,
+  MatSelectModule
+} from '@angular/material';
 import { CarteComponent } from './carte/carte.component';
 import { ReserveComponent } from './reserve/reserve.component';
 import { ContactComponent } from './contact/contact.component';
 import { MainDashComponent } from './main-dash/main-dash.component';
 import { DishTableComponent } from './dish-table/dish-table.component';
-import {MatDividerModule} from '@angular/material/divider';
+import { MatDividerModule } from '@angular/material/divider';
 
 const routes: Routes = [
-  { path: '' , component:  HomeComponent}, // http://localhost:4200
-  { path: 'carte', component: CarteComponent}, // http://localhost:4200/carte
-  { path: 'reserve', component: ReserveComponent}, // http://localhost:4200/reserve
-  { path: 'contact', component: ContactComponent} // http://localhost:4200/contact
+  { path: '', component: HomeComponent }, // http://localhost:4200
+  { path: 'carte', component: CarteComponent }, // http://localhost:4200/carte
+  { path: 'reserve', component: ReserveComponent }, // http://localhost:4200/reserve
+  { path: 'contact', component: ContactComponent } // http://localhost:4200/contact
 ];
 
 @NgModule({
@@ -55,9 +73,10 @@ const routes: Routes = [
     MatInputModule,
     MatFormFieldModule,
     FormsModule,
-    MatNativeDateModule
+    MatNativeDateModule,
+    MatSelectModule
   ],
-  providers: [{provide: MAT_DATE_LOCALE, useValue: 'fr-FR'}],
+  providers: [{ provide: MAT_DATE_LOCALE, useValue: 'fr-FR' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
